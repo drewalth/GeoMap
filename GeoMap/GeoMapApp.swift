1edf88b2604af6cfa3e7f0f59ad779d155d212a0
Drew Althage <andrew.althage@gmail.com>
2023-03-06 17:21:18 -0700

    Initial Commit

---
+++ GeoMap/GeoMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoMapApp.swift
+//  GeoMap
+//
+//  Created by Drew Althage on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GeoMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
