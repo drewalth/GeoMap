0e3f42fbfcede752c074c1344615312411c6bcdd
Drew Althage <andrew.althage@gmail.com>
2023-03-07 15:20:21 -0700

    feat: working demo

--- GeoMap/ContentView.swift
+++ GeoMap/ContentView.swift
@@ -5,17 +5,28 @@
 //  Created by Drew Althage on 3/6/23.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    private let geoManager = GeoManager()
+
+    @State var mapInfo: MapInfo?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Group {
+            if let mapData = mapInfo {
+                MapView(
+                    region: mapData.region,
+                    lineCoordinates: mapData.coordinates
+                )
+                .edgesIgnoringSafeArea(.all)
+            } else {
+                Text("no map data")
+            }
+        }.onAppear {
+            mapInfo = geoManager.getMapInfo(from: sourceData)
         }
-        .padding()
     }
 }
 
---
+++ GeoMap/GeoManager.swift
@@ -0,0 +1,52 @@
+//
+//  GeoManager.swift
+//  GeoMap
+//
+//  Created by Drew Althage on 3/6/23.
+//
+
+import Foundation
+import MapKit
+import os
+
+struct GeoManager {
+    private var logger = Logger()
+
+    func getMapInfo(from geomString: String) -> MapInfo {
+        let zoom: CLLocationDistance = 5000 * 2
+
+        let coords = getCoordinates(geomString: geomString)
+        let center = coords[(coords.count / 2) - 1]
+        let region = MKCoordinateRegion(
+            center: center,
+            latitudinalMeters: zoom, longitudinalMeters: zoom
+        )
+
+        return MapInfo(region: region, coordinates: coords)
+    }
+
+    private func getCoordinates(geomString: String) -> [CLLocationCoordinate2D] {
+        var result: [CLLocationCoordinate2D] = []
+        let coords = geomString.split(separator: ",")
+
+        for coord in coords {
+            let val = coord.split(separator: " ")
+            guard let lat = Double(val[1]),
+                  let lon = Double(val[0])
+            else {
+                print("error with lat lon")
+                continue
+            }
+
+            let locVal = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            result.append(locVal)
+        }
+
+        return result
+    }
+}
+
+struct MapInfo {
+    var region: MKCoordinateRegion
+    var coordinates: [CLLocationCoordinate2D]
+}
